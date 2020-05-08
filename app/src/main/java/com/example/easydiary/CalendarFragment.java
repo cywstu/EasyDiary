4810f9669c5988084db5dfc5e6208f86169570ae
cywstu <46858924+cywstu@users.noreply.github.com>
2020-05-09 03:32:41 +0800

    Fragments
    old one not working well, addressed 2 main problems after reimplementing all classes:
    1. shiftmode - the way to disable has changed but not big deal
    2. bottom nav view related class - because many changes compared with older version, command like compile->implementation, and dependencies like appcompat changes its domain(lol), also the class for nav changed lead to a off topic debug
    well at least the info from web like match the appcompat version and put same files in drawable and many more look-like-unrelated reasons was fun

---
+++ app/src/main/java/com/example/easydiary/CalendarFragment.java
@@ -0,0 +1,18 @@
+package com.example.easydiary;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CalendarFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+}
--- app/src/main/java/com/example/easydiary/MainActivity.java
+++ app/src/main/java/com/example/easydiary/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.easydiary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //nav bar at the bottom
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //navigation bar problem
+        //solution from : stackoverflow.com/questions/41352934/force-showing-icon-and-title-in-bottomnavigationview-support-android/47407412
+        bottomNav = findViewById(R.id.navBar);
+        disableShiftMode(bottomNav);
+
+        //navListener: change fragment when click on nav items
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        //start with "recent" fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecentFragment()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_recent:
+                            selectedFragment = new RecentFragment();
+                            break;
+                        case R.id.nav_calender:
+                            selectedFragment = new CalendarFragment();
+                            break;
+                        case R.id.nav_create:
+                            selectedFragment = new CreateFragment();
+                            break;
+                        case R.id.nav_setting:
+                            selectedFragment = new SettingFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+    /*
+    //for testing buttons
+    public void onClick(View view){
+
+        DiaryDB db = new DiaryDB(this);
+        //db.addDiary(diary);
+        ArrayList resultList = db.getAllDiary();
+
+        Toast.makeText(this, (String)resultList.get(0), Toast.LENGTH_LONG).show();
+
+    }
+    */
+
+    @SuppressLint("RestrictedApi")
+    public static void disableShiftMode(BottomNavigationView view) {
+            BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+            menuView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
+            menuView.buildMenuView();
     }
 }
